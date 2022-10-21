4beee5f4c105b4f6cd434ddc1b750de9bf45aa59
talha4t <mahmudtalha80@gmail.com>
2022-10-21 09:53:27 +0600

    added some projects

---
+++ Chapter-01/Examples/08-ConditionalStatementExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConditionalStatementExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            int aNumber = Convert.ToInt16(userInput);
+
+            if (aNumber >= 0)
+            {
+                Console.WriteLine("You have entered a positive number");
+            }
+            else
+            {
+                Console.WriteLine("You have entered a negative number");
+            }
+        }
+    }
+}
---
+++ Chapter-01/Examples/09-ConditionalStatementExample2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConditionalStatementExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter obtained score in physics: ");
+            string userInput = Console.ReadLine();
+
+            double physicsScore = Convert.ToDouble(userInput);
+
+            if (physicsScore > 100 || physicsScore < 0)
+            {
+                Console.WriteLine("You entered score is invalid. " + "Check it and try again");
+                return;
+            }
+            if (physicsScore <= 100 && physicsScore >= 90)
+            {
+                Console.WriteLine("Your Grade is A+ ");
+            }
+            else if (physicsScore < 90 && physicsScore >= 80)
+            {
+                Console.WriteLine("Your Grade is B+ ");
+            }
+            else if (physicsScore < 80 && physicsScore >= 70)
+            {
+                Console.WriteLine("Your Grade is C+ ");
+            }
+            else
+            {
+                Console.WriteLine("Your Grade is F");
+            }
+        }
+    }
+}
