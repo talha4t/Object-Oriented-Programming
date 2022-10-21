f8ca2166fb1602389675a4651a56d722aa343cb0
talha4t <mahmudtalha80@gmail.com>
2022-10-21 06:08:12 +0600

    Update

--- Chapter-01/Examples/03-VariableExample1/Program.cs
+++ Chapter-01/Examples/03-VariableExample1/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 
-
-
-
-
 namespace VariableExample1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string firstName = "Sahel";
-            string lastName = "Rana";
+            string firstName = "Talha";
+            string lastName = "Mahmud";
             Console.WriteLine(firstName);
             Console.WriteLine(lastName);
             string fullName = firstName + " " + lastName;
--- Chapter-01/Examples/04-VaribaleExample2/Program.cs
+++ Chapter-01/Examples/04-VaribaleExample2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace VariableExample2
+namespace VarbiableExample2
 {
     class Program
     {
@@ -9,25 +9,24 @@ namespace VariableExample2
             byte aByte = 12;
             short aSmallValue = 45;
             int noOfStudents = 100;
-            int numberOfRemainingOver = 23;
-            long population = 340000000000;
+            int noOfReaminingOver = 23;
+            long population = 340000000;
 
             double salaryAmount = 45000.50;
             float valueOfSomething = 78.98f;
             decimal divisionResult = 34.43m;
-
 
             double mathObtainedScore = 98.5;
             double physicsObtainedScore = 86;
             double chemistryObtainedScore = 88;
 
             int noOfSubjects = 3;
-
             double totalObtainedScore = mathObtainedScore + physicsObtainedScore + chemistryObtainedScore;
 
             double avgObtainedScore = totalObtainedScore / noOfSubjects;
 
             Console.WriteLine(avgObtainedScore);
         }
+
     }
 }
