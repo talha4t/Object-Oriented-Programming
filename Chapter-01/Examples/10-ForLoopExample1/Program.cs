24980292fe6df8a637ebe0df9d7ccc20a9a3e5f9
talha4t <mahmudtalha80@gmail.com>
2022-10-22 20:13:32 +0600

    some projects

---
+++ Chapter-01/Examples/10-ForLoopExample1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoopExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine("OOP concept is very essential");
+            }
+        }
+    }
+}
---
+++ Chapter-01/Examples/11-ForLoopExample2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ForLoopExample2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
