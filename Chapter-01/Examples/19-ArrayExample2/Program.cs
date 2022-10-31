7f6ada7bfe9ddae27bc1b5fff56d217a29f369a0
Talha Mahmud <mahmudtalha80@gmail.com>
2022-10-31 20:18:31 +0600

    19 Array Example2

---
+++ Chapter-01/Examples/19-ArrayExample2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace _19_ArrayExample2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("**This program shows only even numbers " +
+                "of your number list**");
+            Console.WriteLine("How many number will you enter: ");
+            int arraysize = Convert.ToInt32(Console.ReadLine());
+
+            int[] allNumber = new int[arraysize];
+            for (int index = 0; index < arraysize; index++)
+            {
+                Console.WriteLine("Enter number " + (index + 1));
+                int aNumber = Convert.ToInt32(Console.ReadLine());
+
+                allNumber[index] = aNumber;
+            }
+
+            Console.WriteLine("Here even numbers are:");
+            for (int index = 0; index < arraysize; index++)
+            {
+                int aNumber = allNumber[index];
+                if (aNumber % 2 == 0)
+                {
+                    Console.Write(aNumber + " ");
+                }
+            }
+
+        }
+    }
+}
