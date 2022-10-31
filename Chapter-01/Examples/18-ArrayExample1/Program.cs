02829c71e731ab68633f6a76f05cf6f22fb8d5f9
Talha Mahmud <mahmudtalha80@gmail.com>
2022-10-31 19:20:38 +0600

    18

---
+++ Chapter-01/Examples/18-ArrayExample1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace _18_ArrayExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            numbers[0] = 12;
+            numbers[1] = 100;
+            numbers[2] = 34;
+
+            int aNumber = numbers[0];
+            Console.WriteLine(aNumber);
+
+            aNumber = numbers[1];
+            Console.WriteLine(aNumber);
+
+            aNumber = numbers[2];
+            Console.WriteLine(aNumber);
+        }
+    }
+}
