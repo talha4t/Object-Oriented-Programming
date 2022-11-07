8769f3316e372228df3b585d0857bc4d9bf58f6b
Talha Mahmud <mahmudtalha80@gmail.com>
2022-11-07 15:35:08 +0600

    added file

---
+++ Chapter-01/Examples/23-HashSetExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _23_hashSetExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<string> allRegistrationNo = new HashSet<string>();
+
+
+            allRegistrationNo.Add("11-00-001");
+            allRegistrationNo.Add("11-00-005");
+            allRegistrationNo.Add("22-00-001");
+            allRegistrationNo.Add("11-11-001");
+            allRegistrationNo.Add("11-00-001"); // duplicate data
+
+            foreach (string aRegiNo in allRegistrationNo)
+            {
+                Console.WriteLine(aRegiNo);
+            }
+        }
+    }
+}
