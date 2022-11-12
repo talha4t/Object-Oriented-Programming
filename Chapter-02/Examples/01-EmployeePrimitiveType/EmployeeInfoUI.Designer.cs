1dd839787758b89c08be695b034aa3d4379f2642
Talha Mahmud <mahmudtalha80@gmail.com>
2022-11-12 18:08:51 +0600

    Employee Primititve Type

---
+++ Chapter-02/Examples/01-EmployeePrimitiveType/EmployeeInfoUI.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _01_EmployeePrimitiveType
+{
+    partial class EmployeeInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveAndClearButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.showSavedDataButton = new System.Windows.Forms.Button();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // saveAndClearButton
+            // 
+            this.saveAndClearButton.Location = new System.Drawing.Point(212, 280);
+            this.saveAndClearButton.Name = "saveAndClearButton";
+            this.saveAndClearButton.Size = new System.Drawing.Size(150, 23);
+            this.saveAndClearButton.TabIndex = 0;
+            this.saveAndClearButton.Text = "Save and Clear";
+            this.saveAndClearButton.UseVisualStyleBackColor = true;
+            this.saveAndClearButton.Click += new System.EventHandler(this.saveAndClearButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(331, 88);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(226, 22);
+            this.idTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Salary Amount";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(331, 140);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(226, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // showSavedDataButton
+            // 
+            this.showSavedDataButton.Location = new System.Drawing.Point(415, 280);
+            this.showSavedDataButton.Name = "showSavedDataButton";
+            this.showSavedDataButton.Size = new System.Drawing.Size(142, 23);
+            this.showSavedDataButton.TabIndex = 0;
+            this.showSavedDataButton.Text = "Show Saved Data";
+            this.showSavedDataButton.UseVisualStyleBackColor = true;
+            this.showSavedDataButton.Click += new System.EventHandler(this.showSavedDataButton_Click);
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(331, 204);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 22);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // EmployeeInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showSavedDataButton);
+            this.Controls.Add(this.saveAndClearButton);
+            this.Name = "EmployeeInfoUI";
+            this.Text = "Employee Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showSavedDataButton;
+        private System.Windows.Forms.Button saveAndClearButton;
+    }
+}
+
---
+++ Chapter-02/Examples/01-EmployeePrimitiveType/EmployeeInfoUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_EmployeePrimitiveType
+{
+    public partial class EmployeeInfoUI : Form
+    {
+        private string id;
+        private string name;
+        private double salary;
+        public EmployeeInfoUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveAndClearButton_Click(object sender, EventArgs e)
+        {
+            id = idTextBox.Text;
+            name = nameTextBox.Text;
+            salary = Convert.ToDouble(salaryTextBox.Text);
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            salaryTextBox.Text = "";
+        }
+
+        private void showSavedDataButton_Click(object sender, EventArgs e)
+        {
+            idTextBox.Text = id;
+            nameTextBox.Text = name;
+            salaryTextBox.Text = salary.ToString();
+        }
+    }
+}
