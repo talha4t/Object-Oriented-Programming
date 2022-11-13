2d9a653c2be0c6a4f4d37c6fa061a5196ee5460a
Talha Mahmud <mahmudtalha80@gmail.com>
2022-11-13 22:21:00 +0600

    Change Maker Example 2

---
+++ Chapter-02/Examples/05-ChangeMakerExample2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05_ChangeMakerExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.name = "Talha";
+            st1.cGPA = 4.00;
+            Console.WriteLine("Before: " + st1.name + " " + st1.cGPA + " " + st1.ageInYear);
+
+            ChangeTheStudent(st1);
+
+            Console.WriteLine("After: " + st1.name + " " + st1.cGPA + " " + st1.ageInYear);
+        }
+        public static void ChangeTheStudent(Student aStudent)
+        {
+            aStudent.name = "Pavel";
+            aStudent.cGPA = 3.89;
+            aStudent.ageInYear = 19;
+        }
+    }
+}
---
+++ Chapter-02/Examples/05-ChangeMakerExample2/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ChangeMakerExample2
+{
+    internal class Student
+    {
+        public string name;
+        public double cGPA;
+        public int ageInYear;
+    }
+}
