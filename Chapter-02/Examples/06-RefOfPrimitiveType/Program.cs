53c051ca15c68fdb9ff1612d1cdc47872ddd28fa
Talha Mahmud <mahmudtalha80@gmail.com>
2022-11-13 22:30:29 +0600

    Ref Of Primitive Type

---
+++ Chapter-02/Examples/06-RefOfPrimitiveType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace _06_RefOfPrimitveType
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+
+            Console.WriteLine("a = " + a + ", b = " + b);
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine("a = " + a + ", b = " + b);
+            
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
