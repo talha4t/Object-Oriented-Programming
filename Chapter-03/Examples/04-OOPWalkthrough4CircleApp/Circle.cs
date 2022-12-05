70aa265938e790a237c00d1ba3de285dddc293d2
talha4t <mahmudtalha80@gmail.com>
2022-12-05 20:08:35 +0600

    Circle App

---
+++ Chapter-03/Examples/04-OOPWalkthrough4CircleApp/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_OOPWalkthrough4CircleApp
+{
+    internal class Circle
+    {
+        public double radius;
+        public double GetDiameter()
+        {
+            return 2 * radius;
+        }
+        public double GetPerimeter()
+        {
+            return Math.PI * GetPerimeter();
+        }
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
---
+++ Chapter-03/Examples/04-OOPWalkthrough4CircleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using _04_OOPWalkthrough4CircleApp;
+using System;
+
+namespace OOPWalkthrough4CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter the radius of a circle: ");
+                string radiusInString = Console.ReadLine();
+                double radius = Convert.ToDouble(radiusInString);
+                Circle aCircle = new Circle();
+                aCircle.radius = radius;
+                Console.WriteLine("Diameter: " + aCircle.GetDiameter);
+                Console.WriteLine("Perimeter: " + aCircle.GetPerimeter);
+                Console.WriteLine("Area: " + aCircle.GetArea());
+                Console.WriteLine();
+
+                Console.WriteLine()
+            }
+        }
+    }
+}
