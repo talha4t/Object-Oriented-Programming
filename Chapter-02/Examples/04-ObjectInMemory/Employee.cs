57fda559f81152821f483eb8cb22598b86e590bb
talha4t <mahmudtalha80@gmail.com>
2022-12-25 15:52:12 +0600

    Chapter 02

---
+++ Chapter-02/Examples/04-ObjectInMemory/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ObjectInMemory
+{
+    internal class Employee
+    {
+        public string id;
+        public string name;
+        public int salaryAmount;
+
+    }
+}
---
+++ Chapter-02/Examples/04-ObjectInMemory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_ObjectInMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+
+            employee1.id = "emp-212";
+            employee1.name = "Talha";
+            employee1.salaryAmount = 50000;
+
+            Employee employee2 = new Employee();
+
+            employee2.id = "emp-100";
+            employee2.name = "Mahmud";
+            employee2.salaryAmount = 500000;
+        }
+    }
+}
